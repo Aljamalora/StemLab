eeefea8299eb7023009eabefaf0dd4ab6f90a2bf
aljamalora <aljamalora@gmail.com>
2026-05-11 23:33:16 +0200

    Auto-generated commit

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { StudioPageComponent } from './features/studio/pages/studio-page/studio-page.component';
-
-export const routes: Routes = [
-  {
-    path: '',
-    component: StudioPageComponent
-  }
-];
